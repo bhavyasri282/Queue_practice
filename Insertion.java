c9dc6f7a1e1d634fd8e34c787247184eca465b7b
Bhavya Sri <24r21a6767@mlrit.ac.in>
2026-09-21 13:19:15 +0530

    Added insertion of elements

---
+++ Insertion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Insertion{
+    public static void main(String args[]){
+        Queue<Integer> q=new LinkedList<>();
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+        System.out.println(q);
+    }
+}
